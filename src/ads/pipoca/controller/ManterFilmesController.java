e25f87bb8feb1a04c3e432b52bad596863e3db08
rguirra <rodrigoguirratrindade@gmail.com>
2020-04-19 22:33:24 -0300

    Identação de código, teste rodrigo

--- src/ads/pipoca/controller/ManterFilmesController.java
+++ src/ads/pipoca/controller/ManterFilmesController.java
@@ -82,13 +82,9 @@ public class ManterFilmesController extends HttpServlet {
 		case "excluir":
 			break;
 		}
-
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
